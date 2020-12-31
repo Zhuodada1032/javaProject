209aa861e5059bde03b755d3efc7032df3db23d0
Zhuodada <Zhuodada1032@163.com>
2020-12-31 16:20:21 +0800

    BlackBeacon 网站应用 SpringBoot_MyBatis_MySQL 0.0.1 TEST  前后端分离 ------- BlackBeacon0.0.1 TEST

---
+++ BlackBeacon/src/test/java/com/zhuo/blackbeacon/BlackbeaconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuo.blackbeacon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackbeaconApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
