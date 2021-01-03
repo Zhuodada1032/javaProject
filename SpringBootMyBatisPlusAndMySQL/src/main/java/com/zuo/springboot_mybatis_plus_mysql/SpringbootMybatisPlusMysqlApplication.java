32f85ef180617271b5a4f794ab6e71ee25f0ef83
Zhuodada <Zhuodada1032@163.com>
2021-01-03 21:29:23 +0800

    springboot_mybatis_plus_mysql 测试版本 MyBatisPlus测试使用

---
+++ SpringBootMyBatisPlusAndMySQL/src/main/java/com/zuo/springboot_mybatis_plus_mysql/SpringbootMybatisPlusMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.zuo.springboot_mybatis_plus_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisPlusMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisPlusMysqlApplication.class, args);
+    }
+
+}
---
+++ SpringBootMyBatisPlusAndMySQL/src/test/java/com/zuo/springboot_mybatis_plus_mysql/SpringbootMybatisPlusMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuo.springboot_mybatis_plus_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisPlusMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
